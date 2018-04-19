77b386c87923d1a05b0c3af80c13abcf66ec3354
camilaszmedeiros <38050752+camilaszmedeiros@users.noreply.github.com>
2018-04-19 09:58:38 -0300

    Update Exercício Programa.py

--- Exercício Programa.py
+++ Exercício Programa.py
@@ -2,8 +2,13 @@
 #==================================================================================================================================#     
 #Variáveis
 i=1
-lista_de_comandos=[0,1,2,3,4]
+lista_de_comandos=[0,1,2,3,4,5]
 lista_de_produtos=dict()
+import json
+#==================================================================================================================================#
+#chamando estoque
+with open('arquivo.txt','r') as arq:
+    lista_de_produtos = json.loads(arq.read())
 #==================================================================================================================================#
 #Print das opções do programa
 while i!=0:
@@ -24,11 +29,12 @@ while i!=0:
     elif opcao==1:
        nome_do_produto=input("Nome do produto:")
        if nome_do_produto not in lista_de_produtos: 
+           lista_de_produtos[nome_do_produto] = {}
            quantidade_inicial=int(input("Quantidade inicial:"))
            if quantidade_inicial<0:
                print("A quantidade inicial não pode ser negativa.")
            else:
-               lista_de_produtos[nome_do_produto]=quantidade_inicial
+               lista_de_produtos[nome_do_produto]['quantidade']=quantidade_inicial
        else:
            print("Produto já está cadastrado.")
 #==================================================================================================================================#      
@@ -47,9 +53,21 @@ while i!=0:
            print ("Elemento não encontrado.")
        elif nome_do_produto in lista_de_produtos:
            nova_quantidade=int(input("Nova quantidade:"))
-           lista_de_produtos[nome_do_produto]+=nova_quantidade
+           lista_de_produtos[nome_do_produto]['quantidade']+=nova_quantidade
 #==================================================================================================================================#          
 #Opção imprimir estoque da lista
     elif opcao==4:
         for k in lista_de_produtos:
-            print("{0}:{1}".format(k, lista_de_produtos[k]))
+            print("{0}:{1}".format(k, lista_de_produtos[k])) 
+            
+            
+            
+            
+            
+            
+            
+
+original = json.dumps(lista_de_produtos, sort_keys = True, indent=4)
+with open('arquivo.txt','w') as arq:
+    arq.write(original)
+    
